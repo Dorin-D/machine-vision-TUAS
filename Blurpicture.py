586fbb5760e1114d09e62273792b962171910b7f
Double D <dorin.doncenco@edu.turkuamk.fi>
2020-04-10 12:13:17 +0300

    Modified blurpicture.py to accept GUI

--- Blurpicture.py
+++ Blurpicture.py
@@ -1,42 +1,36 @@
 import cv2, sys
+import os.path
 
-# get filename and kernel size values from command line
-filename = sys.argv[1]
-k = int(sys.argv[2])
+def blur_picture(filename, blackBG, k):
+	# read and display the original image
+	image = cv2.imread(filename = filename)
+	if not(blackBG):
+		image = cv2.bitwise_not(image)
+	# blur and grayscale before thresholding
+	blur = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2GRAY)
+	blur = cv2.GaussianBlur(
+		src = blur, 
+		ksize = (k, k), 
+		sigmaX = 0)
 
-# read and display the original image
-image = cv2.imread(filename = filename)
-cv2.namedWindow(winname = "original", flags = cv2.WINDOW_NORMAL)
-cv2.imshow(winname = "original", mat = image)
-cv2.waitKey(delay = 0)
+	# perform adaptive thresholding 
+	(t, maskLayer) = cv2.threshold(src = blur, 
+		thresh = 0, 
+		maxval = 255, 
+		type = cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-# blur and grayscale before thresholding
-blur = cv2.cvtColor(src = image, code = cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(
-    src = blur, 
-    ksize = (k, k), 
-    sigmaX = 0)
+	# make a mask suitable for color images
+	mask = cv2.merge(mv = [maskLayer, maskLayer, maskLayer])
+	# use the mask to select the "interesting" part of the image
+	sel = cv2.bitwise_and(src1 = image, src2 = mask)
 
-# perform adaptive thresholding 
-(t, maskLayer) = cv2.threshold(src = blur, 
-    thresh = 0, 
-    maxval = 255, 
-    type = cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+	# output the result
+	outputFile = "imagewithblur.png"
+	cv2.imwrite(outputFile, img = sel)
+	return outputFile
 
-# make a mask suitable for color images
-mask = cv2.merge(mv = [maskLayer, maskLayer, maskLayer])
-
-cv2.namedWindow(winname = "mask", flags = cv2.WINDOW_NORMAL)
-cv2.imshow(winname = "mask", mat = mask)
-cv2.waitKey(delay = 0)
-
-# use the mask to select the "interesting" part of the image
-sel = cv2.bitwise_and(src1 = image, src2 = mask)
-
-# display the result
-cv2.namedWindow(winname = "selected", flags = cv2.WINDOW_NORMAL)
-cv2.imshow(winname = "selected", mat = sel)
-cv2.waitKey(delay = 0)
-
-## find out why instead of the selected, the original was saved
-cv2.imwrite("imagewithblur.png", img = sel)
+		
+if __name__ == "__main__":
+	filename = sys.argv[1]
+	k = int(sys.argv[2])
+	blur_picture(filename, False, k)
